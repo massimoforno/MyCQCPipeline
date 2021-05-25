107027cc989978156ca0cacf440ca4d74619ea95
mforno <massimo.forno@gmail.com>
2021-05-25 12:52:51 +0200

    First Commit of CQCPipeline Eclipse Project

---
+++ src/main/java/com/javahelps/cqcpipeline/CQCPipelineServlet.java
@@ -0,0 +1,48 @@
+package com.javahelps.cqcpipeline;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldServlet 
+ */
+@WebServlet("/saytime")
+public class CQCPipelineServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+      
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CQCPipelineServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.print("<html><body><h1 align='center'> My SUPER Continuous Quality "
+        		+ "and Compliance Pipeline is up and running on:"
+         + "</h1></body></html>");
+        out.print("<html><body><h1 align='center'>" +
+        new Date().toString() + "</h1></body></html>");
+        
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+    }
+
+}
