0b77b93277183df362e6f3210b38ee508b7bc0d2
mforno <massimo.forno@gmail.com>
2021-05-26 09:01:23 +0200

    jakarta.servlet.* instead of java.servlet.*

--- src/main/java/com/javahelps/cqcpipeline/CQCPipelineServlet.java
+++ src/main/java/com/javahelps/cqcpipeline/CQCPipelineServlet.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 
-import jakarta.servlet.ServletException;
+import jakarta.servlet.*;
+//import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
