d18a49d8851105edc136950bbfbeb5c17843c02a
mforno <massimo.forno@gmail.com>
2021-05-26 10:07:37 +0200

    zzz

--- src/main/java/com/javahelps/cqcpipeline/CQCPipelineServlet.java
+++ src/main/java/com/javahelps/cqcpipeline/CQCPipelineServlet.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 
-import javax.servlet.*;
+import jakarta.servlet.*;
 //import jakarta.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class HelloWorldServlet 
